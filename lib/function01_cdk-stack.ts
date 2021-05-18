5c572bbcc0b4c63744cea38a12ece0360a5d44bb
Adriano Sastre Vieira <adrianosastre@inatel.br>
2021-05-18 15:46:34 -0300

    aws-apigateway, lambda, lambda-nodejs added to package.json; iac code added to the first cdk stack

--- lib/function01_cdk-stack.ts
+++ lib/function01_cdk-stack.ts
@@ -1,9 +1,32 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as lambdaNodeJS from '@aws-cdk/aws-lambda-nodejs';
+import * as apigateway from '@aws-cdk/aws-apigateway';
 
 export class Function01CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const handler = new lambdaNodeJS.NodejsFunction(this, 'Function01', {
+      entry: 'lambda/fuction1.js',
+      handler: 'handler',
+      bundling: {
+        minify: true,
+        sourceMap: false,
+      },
+    });
+
+    const api = new apigateway.RestApi(this, 'hello-api', {
+      restApiName: 'Hello Service',
+      description: 'This is the Hello service'
+    });
+
+    const gettHelloIntegration = new apigateway.LambdaIntegration(handler, {
+      requestParameters: {
+        'application/json': '{statusCode: "200"}'
+      },
+    });
+
+    api.root.addMethod('GET', gettHelloIntegration);
   }
 }
