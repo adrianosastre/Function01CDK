10ccaca038598fe5131fc501ed794d2c74411243
Adriano Sastre Vieira <adrianosastre@inatel.br>
2021-05-20 07:06:09 -0300

    .gitignore changed to include js files, lambda folder added

---
+++ lambda/function01.js
@@ -0,0 +1,39 @@
+const AWS = require('aws-sdk');
+const AWSXRAY = require('aws-xray-sdk-core');
+
+const xRay = AWSXRAY.captureAWS(require('aws-sdk')); // toda requisição poderá ser monitorada pelo aws x-ray
+
+exports.handler = async function(event, context) {
+    // console.log(event);
+    // console.log(context);
+
+    const apiGwRequestId = event.requestContext.requestId; // request id da api gtw (chamou o lambda)
+    const lambdaRequestId = context.awsRequestId; // request id do lambda
+
+    console.log(`API Gateway Request Id: ${apiGwRequestId} Lambda Request Id: ${lambdaRequestId}`);
+
+    const method = event.httpMethod
+    if (method === 'GET') {
+        if (event.path == '/') {
+            return {
+                statusCode: 200,
+                headers: {},
+                body: JSON.stringify({
+                    message: 'Hello World! apiGwRequestId lambdaRequestId',
+                    apiGwRequestId: apiGwRequestId,
+                    lambdaRequestId: lambdaRequestId,
+                }),
+            };
+        }
+    }
+
+    return {
+        statusCode: 400,
+        headers: {},
+        body: JSON.stringify({
+            message: 'Bad Request!',
+            apiGwRequestId: apiGwRequestId,
+            lambdaRequestId: lambdaRequestId,
+        }),
+    };
+}
